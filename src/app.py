1eca7ff69a3a61d178d71feeb045a073ebac550e
Isaac I. Martinez M <48190882+isaacismaelx14@users.noreply.github.com>
2021-06-15 21:01:54 -0400

    Creating cli functions

--- src/app.py
+++ src/app.py
@@ -31,13 +31,19 @@ _CONFIG = {
     "component": _COMPONENT_CONFIG
 }
 
+_DESTINATION = None
+_FILE_NAME = None
 
+class Exit(Exception):
+    pass
+
+# Create a folder for a the components
 def create_component_folder(filePath: Path, fn):
     if(filePath.exists() == False):
         os.mkdir(filePath)
         print(f'{blue(f"{fn} folder")} {green("created")}')
 
-
+# Create the files of the component
 def create_files(dest: Path, fn: str, opt):
     select_type = opt['style_type']
     ft = opt['lang']
@@ -64,7 +70,9 @@ def create_files(dest: Path, fn: str, opt):
     if use_test:
         filesController.create_test_js(fileAbsoulte, fn, ft, use_index)
 
-
+#   Print a message with option (Yes or No) and return the answer in str
+#   if you press a valid as Y, Yes, N or No, this functin return "True"
+#   or "False". But if you type a not valid value return "Invalid"
 def accept(message: str) -> str:
     response = input(
         f'{message} {green("(yes/no)")}: ').lower()
@@ -76,49 +84,55 @@ def accept(message: str) -> str:
         print(f'"{response}" is not supported, please try again...')
         return "Invalid"
 
-
-def create_directory(dir: Path):
+# Create a directory and  go to directory and recive a funtion
+# to now were comeback when finish
+def create_directory(dir: Path, callback):
     os.makedirs(dir, exist_ok=True)
-    main()
+    callback()
 
-
-def no_exist_directory(dir: Path):
+# Print a message asking if you want to create a directory
+def no_exist_directory():
+    dir = _DESTINATION
     message = f"{yellow(f'{dir}')} don\'t exist, {blue('do you want to create?')}"
     resp = accept(message)
 
     if(resp == "True"):
-        create_directory(dir.absolute())
+        create_directory(dir.absolute(), get_directory)
     elif(resp == "False"):
         print('please execute the CLI again and enter another directory')
-        exit()
+        Exit(0)
     else:
         no_exist_directory(dir)
 
-
-def get_directory(dest: Path, fn: str):
+# detect if is a directory or no and if the directory exists
+def get_directory():
+    dest = Path(_DESTINATION)
+    fn = _FILE_NAME
     if dest.is_dir():
         create_files(dest.absolute(), fn, _COMPONENT_CONFIG)
-    elif dest.is_file():
+    elif dest.is_file() or dest.is_socket():
         raise ErrorExp(red(f'{dest} is not a directory'))
     else:
-        no_exist_directory(dest)
+        no_exist_directory()
 
-
+# set the cofiguration from the config file 
 def set_config(config):
     main_keys = ['dir', 'lang']
     for key in main_keys:
         if key in config:
             _CONFIG[key] = config[key]
 
-
+# Print warnings while set the components config
 def _compoents_warnings(component_config):
     if(component_config['component_file_type'] == 'jsx' and component_config['lang'] == 'ts'):
         print(yellow(
             'warning:'), 'Your filetype is .ts and your components .jsx (config file)')
         press_e_to_exit_and_c_to_continue()
     if(component_config['component_file_type'] == 'tsx' and component_config['lang'] == 'js'):
-        print(yellow('warning:'), 'Your filetype is .js and your components .tsx (config file)')
+        print(yellow('warning:'),
+            'Your filetype is .js and your components .tsx (config file)')
         press_e_to_exit_and_c_to_continue()
+
 
 def set_component_config(config):
     keys = ['lang', 'component_file_type', 'style_type',
@@ -138,8 +152,10 @@ def set_component_config(config):
                     if component_config['component_file_type'] == 'tsx':
                         _COMPONENT_CONFIG['lang'] = 'ts'
 
+
 def press_e_to_exit_and_c_to_continue():
-    res = input(f"Press {red('[x]')} to exit and {green('[c]')} to continue: ").lower()
+    res = input(
+        f"Press {red('[x]')} to exit and {green('[c]')} to continue: ").lower()
     if(res == 'c'):
         pass
     elif(res == 'x'):
@@ -147,7 +163,7 @@ def press_e_to_exit_and_c_to_continue():
     else:
         raise ErrorExp(red(f'"{res}" is not a valid value'))
 
-
+#Get data from config file
 def get_config():
     config = read_cofig.start()
 
@@ -161,25 +177,58 @@ def get_data_config():
         'dest': Path(_CONFIG['dir'])
     }
 
-def local_cli():
+# detect if a string is empty
+def is_empty(string: str)-> bool:
+    new_str = string.strip()
+    if(len(new_str) == 0):
+        return True
+    return False
+
+
+def create_component():
+    component = input('Type the name of the component: ')
+    main_task(component)
+
+
+def get_selection_cli(selection):
+    clearConsole()
+    if(is_empty(selection)):
+        print('[Default]')
+        selection = '1'
+    if(selection == "1"):
+        print(green('Create component'))
+        print('\n')
+        create_component()
+    elif(selection == "2"):
+        create_config.start(options.start())
+    elif(selection == 'q'):
+        Exit(0)
+
+
+def local_cli()->str:
     print(f"{blue('Project Manager')} {green('CLI')}")
     print(f"Selecte an option:")
-    print("\n")
-    print("""
-************************************************
-******     [1] Create component          *******
-******     [2] Create config file       *******
-************************************************
+    print(f"""
+{yellow('******************************************************')}
+{yellow('******')}           {red('[1]')} {blue('Create component')}          {yellow('*******')}
+{yellow('******')}           {red('[2]')} {blue('Create config file')}        {yellow('*******')}
+{yellow('******')}           {red('[q]')} {blue('Exit')}                      {yellow('*******')}
+{yellow('******************************************************')}
         """)
-    valut = input('Select a value (default 1): ')
+    return input(f'Select a value {red("(default 1)")}: ').lower()
 
 
-def main_task(component,arg_dest):
+def main_task(component, arg_dest=None):
+    global _DESTINATION
+    global _FILE_NAME
     get_config()
     config = get_data_config()
     dest = arg_dest if arg_dest else config['dest']
     clearConsole()
-    get_directory(dest, component)
+    _DESTINATION = dest
+    _FILE_NAME = component
+    get_directory()
+
 
 def main():
     clearConsole()
@@ -193,11 +242,16 @@ def main():
         elif(component == None and arg_config):
             create_config.start(options.start())
         else:
-            local_cli()
+            get_selection_cli(local_cli())
     except ErrorExp as e:
         clearConsole()
         print(e, file=stderr)
         exit(1)
+    except KeyboardInterrupt:
+        clearConsole()
+    except Exit as code:
+        clearConsole()
+        exit(code)
 
 
 if __name__ == '__main__':
