a4a0c72f7fd7f5d7d9578d7fedba356a8cbbf9e8
Isaac Martinez <isaacismaelx14@gmail.com>
2021-06-26 18:20:31 -0400

    New version

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="create_project",
-    version="0.0.1",
-    # package_dir = {
-    #     "create_project":"create_project",
-    #     "create_project.functions":"create_project/functions",
-    #     "create_project.config":"create_project/config"
-    # },
-    # packages=["create_project", "create_project.functions", "create_project.config"],
+    name="create_component",
+    version="1.2",
     packages=find_packages(),
     url="https://github.com/isaacismaelx14/Project-Manager-CLI/",
     license="MIT License",
@@ -16,6 +10,6 @@ setup(
     author_email="isaacismaelx14@gmail.com",
     description="With this script you have a template for create react components with some default files",
     entry_points={
-        'console_scripts': ['create_project = create_project.app:main'],
+        'console_scripts': ['create_component = create_component.app:main'],
     }
 )
